fdb03cb93c5e73bfd580c2c0f4f52a12d52c70c1
Yegor Gera <g3eraa@gmail.com>
2025-09-26 01:16:08 +0300

    lab3 final

--- lab3var7/dvoichnyifile.cpp
+++ lab3var7/dvoichnyifile.cpp
@@ -1,4 +1,57 @@
-#include <iostream>
-#include <fstream>
 #include "modules.h"
+#include <fstream>
+#include <iostream>
 using namespace std;
+const int NMAX = 1000;
+
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    cout << "Недостаточно входных параметров" << endl;
+    return 1;
+  }
+
+  char *textfile = argv[1];
+  char *binfile = argv[2];
+
+  short data[NMAX * 3];
+  int n = 0;
+  int rec_1, rec_2;
+
+  if (!input(&n, data, textfile)) {
+    return 1;
+  }
+
+  if (!BinOutput(n, data, binfile)) {
+    return 1;
+  }
+
+  short read_data[NMAX * 3];
+  int records_count = 0;
+
+  cout << "Данные до изменений:" << endl;
+  if (!BinInput(read_data, &records_count, binfile)) {
+    return 1;
+  }
+  for (int i = 0; i < records_count; i++) {
+    cout << "Запись " << i + 1 << ": " << read_data[i * 3] << " "
+         << read_data[i * 3 + 1] << " " << read_data[i * 3 + 2] << endl;
+  }
+
+  cout << "Введите номера записей для обмена: ";
+  cin >> rec_1 >> rec_2;
+
+  if (!Swap(n, binfile, (rec_1 - 1), (rec_2 - 1))) {
+    return 1;
+  }
+
+  cout << "Данные после изменений:" << endl;
+  if (!BinInput(read_data, &records_count, binfile)) {
+    return 1;
+  }
+  for (int i = 0; i < records_count; i++) {
+    cout << "Запись " << i + 1 << ": " << read_data[i * 3] << " "
+         << read_data[i * 3 + 1] << " " << read_data[i * 3 + 2] << endl;
+  }
+
+  return 0;
+}
--- lab3var7/modules.cpp
+++ lab3var7/modules.cpp
@@ -1,4 +1,120 @@
-#include <iostream>
-#include <cstdio>
 #include "modules.h"
+#include <fstream>
+#include <iostream>
 using namespace std;
+
+using Record = short[3];
+size_t rec_size = 3 * sizeof(short);
+
+int input(int *n, short x[], char *fname);
+int BinOutput(int n, short x[], char *fname);
+int BinInput(short x[], int *n, char *fname);
+int Swap(int n, char *fname, int record1, int record2);
+int input(int *n, short x[], char *fname) {
+  ifstream file(fname);
+  if (!file.is_open()) {
+    cout << "Невозможно открыть файл '" << fname << "'" << endl;
+    return 0;
+  }
+  file >> *n;
+  if (file.fail()) {
+    cout << "Ошибка чтения из файла '" << fname << "'" << endl;
+    return 0;
+  }
+  if (*n < 2) {
+    cout << "Кол-во записей должно быть хотя бы 2" << endl;
+    return 0;
+  }
+
+  for (int i = 0; i < *n; i++) {
+    for (int j = 0; j < 3; j++) {
+      file >> x[i * 3 + j];
+      if (file.fail()) {
+        cout << "Ошибка чтения из файла '" << fname << "'" << endl;
+        return 0;
+      }
+    }
+  }
+  file.close();
+  return 1;
+}
+
+int BinOutput(int n, short x[], char *fname) {
+  ofstream file(fname, ios_base::out | ios_base::binary);
+  if (!file.is_open()) {
+    cout << "Невозможно открыть файл " << fname << endl;
+    return 0;
+  }
+
+  file.write(reinterpret_cast<char *>(x), n * 3 * sizeof(short));
+
+  if (file.fail()) {
+    cout << "Логическая ошибка при записи" << endl;
+    file.close();
+    return 0;
+  }
+  if (file.bad()) {
+    cout << "Критическая ошибка при записи" << endl;
+    file.close();
+    return 0;
+  }
+
+  file.close();
+  return 1;
+}
+
+int BinInput(short x[], int *n, char *fname) {
+  ifstream file(fname, ios_base::in | ios_base::binary);
+  if (!file.is_open()) {
+    cout << "Невозможно открыть файл '" << fname << "'\n";
+    return 0;
+  }
+
+  file.seekg(0, ios_base::end);
+  int total_bytes = file.tellg();
+  *n = total_bytes / rec_size;
+  file.seekg(0, ios_base::beg);
+
+  file.read(reinterpret_cast<char *>(x), *n * rec_size);
+
+  if (file.fail()) {
+    cout << "Ошибка чтения из файла '" << fname << "'\n";
+    return 0;
+  }
+  file.close();
+  return 1;
+}
+int Swap(int n, char *fname, int record1, int record2) {
+  fstream file(fname, ios_base::in | ios_base::out | ios_base::binary);
+  if (!file.is_open()) {
+    cout << "Невозможно открыть файл '" << fname << "'\n";
+    return 0;
+  }
+
+  if (record1 < 0 || record1 >= (n + 1) || record2 < 0 || record2 >= (n + 1)) {
+    cout << "Неверные номера записей\n";
+    return 0;
+  }
+
+  short rec1[3], rec2[3];
+
+  file.seekg(record1 * rec_size, ios_base::beg);
+  file.read(reinterpret_cast<char *>(rec1), rec_size);
+
+  file.seekg(record2 * rec_size, ios_base::beg);
+  file.read(reinterpret_cast<char *>(rec2), rec_size);
+
+  file.seekp(record1 * rec_size, ios_base::beg);
+  file.write(reinterpret_cast<char *>(rec2), rec_size);
+
+  file.seekp(record2 * rec_size, ios_base::beg);
+  file.write(reinterpret_cast<char *>(rec1), rec_size);
+
+  if (file.fail()) {
+    cout << "Ошибка при обмене записей\n";
+    return 0;
+  }
+
+  file.close();
+  return 1;
+}
